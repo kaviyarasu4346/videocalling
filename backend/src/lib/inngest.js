328ff239a99a6c22be6d8865f8a591c1d94ccfec
KaviTech <thangavelkaviyarasu@gmail.com>
2026-04-11 22:30:45 +0530

    importing functions

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "../models/Uer.js";
+import User from "../models/User.js";
 
 
 
@@ -25,7 +25,7 @@ const syncUser = inngest.createFunction(
             await User.create(newUser)
     }
 )
-    const deletUserFromDB = inngest.createFunction(
+    const deleteUserFromDB = inngest.createFunction(
     {id:"delete-user-from-db"},
     {event:"clerk/user.deleted"},
     async ({event}) =>
