fe513453d48cef28e6dc8323e280a3e535942a0e
KaviTech <thangavelkaviyarasu@gmail.com>
2026-04-11 22:14:06 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,19 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import cors from "cors";    
+import {serve} from "inngest/express";
 
 
 
 const app = express()
 
 const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use("/api/inngest",serve({client:inngest,functions}));
+
 
 console.log(ENV.PORT);
 console.log(ENV.DB_URL);
