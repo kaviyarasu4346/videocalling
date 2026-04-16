b33f698fa6e5d515a2c786f0e5826756b622d765
KaviTech <thangavelkaviyarasu@gmail.com>
2026-04-16 07:56:37 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,9 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";    
 import {serve} from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
+import { clerkMiddleware } from '@clerk/express';
+import chatRoutes from "./routes/chatRoutes.js";
+
 
 const app = express()
 
@@ -12,19 +15,17 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(clerkMiddleware());
 app.use("/api/inngest",serve({client:inngest,functions}));
+app.use("/api/chat",chatRoutes);
 
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+
 
 app.get("/health", (req,res) => {
     res.status(200).json({msg:"success from api yess"});
 });
 
-app.get("/books", (req,res) => {
-    res.status(200).json({msg:"this is the books endpoints"});
-});
 
 
 //make our app ready for development
